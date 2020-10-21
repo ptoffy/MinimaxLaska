0e85e48b2b41d134f4b5e8081f7c6b4666503266
Paul Toffoloni <paultoffoloni@gmail.com>
2020-10-21 17:47:40 +0200

    Initial Commit

---
+++ Laska/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Laska
+//
+//  Created by Paul Toffoloni on 21/10/2020.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
